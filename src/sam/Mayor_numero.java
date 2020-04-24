1aed62ce97a8feb46cb6c9487e0444047862b94e
sgcm14 <sgcm14@gmail.com>
2020-04-23 20:09:12 -0500

    modificacion

---
+++ src/sam/Mayor_numero.java
@@ -0,0 +1,28 @@
+package sam;
+
+import java.util.Scanner;
+
+public class Mayor_numero {
+	public static void main(String[] args) {
+	//Dado los números 56, 34, 120  mostrar el mayor de ellos.
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ingrese 1° número: ");
+		int n1=sc.nextInt();
+		System.out.println("Ingrese 2° número: ");
+		int n2=sc.nextInt();
+		System.out.println("Ingrese 3° número: ");
+		int n3=sc.nextInt();
+	if(n1>n2 && n1>n3) {
+		System.out.println("El mayor es "+n1);
+	}
+	else if(n2>n1 && n2>n3) {
+		System.out.println("El mayor es "+n2);
+	}
+	else if(n3>n1 && n3>n2) {
+		System.out.println("-------------------");
+		System.out.println("El mayor es "+n3);
+	}
+		
+		
+	}
+}
--- src/sam/numero_par.java
+++ src/sam/numero_par.java
@@ -2,15 +2,15 @@ package sam;
 
 import java.util.Scanner;
 
-public class numero_par {
+public class Numero_par {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Dado el numero 234 mostrar por la salida si dicho número es o no par.
 			int numero;
 	Scanner sc = new Scanner(System.in);
-	System.out.println("Ingrese número");
+	System.out.println("Ingrese número: ");
 	numero=sc.nextInt();
-	
+	System.out.println("---------------------");
 	if(numero%2==0) {
 		System.out.println("Es par");
 	}else {
